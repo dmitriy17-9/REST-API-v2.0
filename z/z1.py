2a19fb941074b700a42b5c4fbc8dc3ae71220d4c
Dima Matveev <adm.matveeva@yandex.ru>
2021-04-25 20:41:34 +0300

    Приложение REST-API v1.0

--- z/z1.py
+++
@@ -1,6 +0,0 @@
-db_name = input()
-global_init(db_name)
-db_sess = create_session()
-users = db_sess.query(User).filter(User.address == "module_1")
-for user in users:
-    print(user)
--- z/z6.py
+++
@@ -1,6 +0,0 @@
-db_name = input()
-global_init(db_name)
-db_sess = create_session()
-jobs = db_sess.query(Jobs).filter(Jobs.work_size < 20, Jobs.is_finished == 0)
-for job in jobs:
-    print(job)
